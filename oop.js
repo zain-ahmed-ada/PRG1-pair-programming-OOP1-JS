96fd4bdc90a731f4c90ee74ee5e33975d30de78c
zain-ahmed <zain.ahmed@justeattakeaway.com>
2024-10-24 12:24:58 +0100

    Added AdaStudent class

--- oop.js
+++ oop.js
@@ -1,77 +1,118 @@
 class Person {
-  #name;
-  #dateOfBirth;
-  #placeOfBirth;
+	#name;
+	#dateOfBirth;
+	#placeOfBirth;
 
-  constructor(name, dateOfBirth, placeOfBirth) {
-    this.#name = name;
-    this.#dateOfBirth = dateOfBirth;
-    this.#placeOfBirth = placeOfBirth;
-  }
+	constructor(name, dateOfBirth, placeOfBirth) {
+		this.#name = name;
+		this.#dateOfBirth = dateOfBirth;
+		this.#placeOfBirth = placeOfBirth;
+	}
 
-  get name() {
-    return this.#name;
-  }
+	get name() {
+		return this.#name;
+	}
 
-  set name(name) {
-    this.#name = name;
-  }
+	set name(name) {
+		this.#name = name;
+	}
 
-  get dateOfBirth() {
-    return this.#dateOfBirth;
-  }
+	get dateOfBirth() {
+		return this.#dateOfBirth;
+	}
 
-  get placeOfBirth() {
-    return this.#placeOfBirth;
-  }
+	get placeOfBirth() {
+		return this.#placeOfBirth;
+	}
 
-  talk() {
-    return `Hi my name is ${this.name} and I was born in ${this.placeOfBirth}.`;
-  }
+	talk() {
+		return `Hi my name is ${this.name} and I was born in ${this.placeOfBirth}.`;
+	}
 }
 
 class AdaStaff extends Person {
-  #role;
-  #base;
+	#role;
+	#base;
 
-  constructor(name, dateOfBirth, placeOfBirth, role, base) {
-    super(name, dateOfBirth, placeOfBirth);
-    this.#role = role;
-    this.#base = base;
-  }
+	constructor(name, dateOfBirth, placeOfBirth, role, base) {
+		super(name, dateOfBirth, placeOfBirth);
+		this.#role = role;
+		this.#base = base;
+	}
 
-  get base() {
-    return this.#base;
-  }
+	get base() {
+		return this.#base;
+	}
 
-  set base(base) {
-    this.#base = base;
-  }
+	set base(base) {
+		this.#base = base;
+	}
 
-  get role() {
-    return this.#role;
-  }
+	get role() {
+		return this.#role;
+	}
 
-  set role(role) {
-    this.#role = role;
-  }
+	set role(role) {
+		this.#role = role;
+	}
 
-  talk() {
-    return `Hi my name is ${this.name}. I work in ${this.base} and my role is ${this.role}.`;
-  }
+	talk() {
+		return `Hi my name is ${this.name}. I work in ${this.base} and my role is ${this.role}.`;
+	}
 }
 
+class AdaStudent extends Person {
+	#company;
+	#pathway;
+	#yearStarted;
+
+	constructor(name, dateOfBirth, placeOfBirth, company, pathway, yearStarted) {
+		super(name, dateOfBirth, placeOfBirth);
+		this.#company = company;
+		this.#pathway = pathway;
+		this.#yearStarted = yearStarted;
+	}
+
+	get company() {
+		return this.#company;
+	}
+
+	set company(company) {
+		this.#company = company;
+	}
+
+	get pathway() {
+		return this.#pathway;
+	}
+
+	set pathway(pathway) {
+		this.#pathway = pathway;
+	}
+
+	get yearStarted() {
+		return this.#yearStarted;
+	}
+
+	set yearStarted(yearStarted) {
+		this.#yearStarted = yearStarted;
+	}
+
+	talk() {
+		return `Hi my name is ${this.name}. I work at ${this.company} and I started in ${this.yearStarted} and my pathway is ${this.pathway}.`;
+	}
+}
 
 // Testing code
 
-const aqil = new Person("Aqil Hussain", "01/01/2000", "Manchester");
-const steve = new AdaStaff("Steve Rich", "01/01/1998", "Lincoln", "Lecturer", "Manchester");
+//const aqil = new Person("Aqil Hussain", "01/01/2000", "Manchester");
+//const steve = new AdaStaff("Steve Rich", "01/01/1998", "Lincoln", "Lecturer", "Manchester");
+const zain = new AdaStudent("Zain Ahmed", "11/10/2005", "London", "Just Eat", "Software Engineering", "2024");
 
-
-console.log(steve);
-console.log(aqil);
-console.log(steve.talk());
-console.log(aqil.talk());
-console.log(steve.name);
-steve.name = "Stevano Rich";
-console.log(steve.name);
+// console.log(steve);
+// console.log(aqil);
+// console.log(steve.talk());
+// console.log(aqil.talk());
+// console.log(steve.name);
+// steve.name = "Stevano Rich";
+// console.log(steve.name);
+console.log(zain.talk());
