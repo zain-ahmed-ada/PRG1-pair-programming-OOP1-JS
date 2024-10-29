467d1c6571db7f9062ad074e392621133cb8152a
Zain Ahmed <zain.ahmed@ada.ac.uk>
2024-10-29 10:25:48 +0000

    Completed extension task

--- oop.js
+++ oop.js
@@ -116,16 +116,22 @@ class Cohort {
 	}
 
 	remove(removeName) {
-		// for (const member of this.cohort) {
-		// 	if (removeName === member.name) {
-		// 		this.cohort.slice(member, 1);
-		// 	}
-		// }
 		for (let i = 0; i < this.cohort.length; i++) {
-			//if (removeName === member.name) {
-			//this.cohort.slice(i);
+			if (removeName === this.cohort[i].name) {
+				this.cohort.splice(i, 1);
+				i--;
+			}
 		}
 	}
+
+	searchStudent(searchStudent) {
+		for (let i = 0; i < this.cohort.length; i++) {
+			if (searchStudent === this.cohort[i].name) {
+				return `Student found at position: ${i}}`;
+			}
+		}
+	}
+
 	list() {
 		for (const member of this.cohort) {
 			console.log(member.name);
@@ -135,9 +141,9 @@ class Cohort {
 
 // Testing code
 
-//const aqil = new Person("Aqil Hussain", "01/01/2000", "Manchester");
-//const steve = new AdaStaff("Steve Rich", "01/01/1998", "Lincoln", "Lecturer", "Manchester");
-//const zain = new AdaStudent("Zain Ahmed", "11/10/2005", "London", "Just Eat", "Software Engineering", "2024");
+// const aqil = new Person("Aqil Hussain", "01/01/2000", "Manchester");
+// const steve = new AdaStaff("Steve Rich", "01/01/1998", "Lincoln", "Lecturer", "Manchester");
+// const zain = new AdaStudent("Zain Ahmed", "11/10/2005", "London", "Just Eat", "Software Engineering", "2024");
 
 // console.log(steve);
 // console.log(aqil);
@@ -146,14 +152,14 @@ class Cohort {
 // console.log(steve.name);
 // steve.name = "Stevano Rich";
 // console.log(steve.name);
-//console.log(zain.talk());
+// console.log(zain.talk());
 
 const april24 = new Cohort("24-04-LDN-MCR");
+
 const sumilA = new AdaStudent("Sumil Aryin", "22/12/2002", "London", "PwC", "TC", 2024);
 april24.add(sumilA);
-april24.add(new AdaStudent("Jess Fryer", "02/02/1998", "Manchester", "PwC", "TC", 2024));
 
-april24.list();
-april24.remove("Sumil Aryin");
-console.log(" ");
-april24.list();
+const JessF = new AdaStudent("Jess Fryer", "02/02/1998", "Manchester", "PwC", "TC", 2024);
+april24.add(JessF);
+
+// april24.list();
